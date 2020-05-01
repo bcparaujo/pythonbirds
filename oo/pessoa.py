2fe4ac6a3356efef5661b2db3d9bac5a99392a88
bruna <bruna@montra.app>
2020-05-01 03:49:26 -0300

    Criados atributos de instância nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(p.cumprimentar())
+    p.nome = 'Bruna'
+    p.idade = '24'
+    print(f'Nome: {p.nome} - Idade: {p.idade}')
